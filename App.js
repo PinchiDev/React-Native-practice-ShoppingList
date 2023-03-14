ad2b473ba3137e0ec1e4451b378a6e59b7b1eb56
Carmelo Cabezon <89110064+PinchiDev@users.noreply.github.com>
2023-03-14 14:59:32 -0300

    style changes, background img and card spacing

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, FlatList, StyleSheet, Text } from 'react-native';
-
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  ImageBackground,
+} from "react-native";
+const image = {
+  uri: "https://cdn.pixabay.com/photo/2017/11/04/21/09/textile-2918844__340.jpg",
+};
 
 export default function App() {
-  const [grocery, setGrocery] = useState('');
-  const [quantity, setQuantity] = useState('');
+  const [grocery, setGrocery] = useState("");
+  const [quantity, setQuantity] = useState("");
   const [groceryList, setGroceryList] = useState([]);
 
   const handleAddGrocery = () => {
-    if (grocery !== '' && quantity !== '') {
-      setGroceryList([...groceryList, { key: Math.random().toString(), grocery: grocery, quantity: quantity }]);
-      setGrocery('');
-      setQuantity('');
+    if (grocery !== "" && quantity !== "") {
+      setGroceryList([
+        ...groceryList,
+        { key: Math.random().toString(), grocery: grocery, quantity: quantity },
+      ]);
+      setGrocery("");
+      setQuantity("");
     }
   };
 
-  const handleRemoveGrocery = key => {
-    setGroceryList(groceryList.filter(item => item.key !== key));
+  const handleRemoveGrocery = (key) => {
+    setGroceryList(groceryList.filter((item) => item.key !== key));
   };
 
   const renderGroceryItem = ({ item }) => (
@@ -25,35 +38,43 @@ export default function App() {
         <Text style={styles.groceryText}>{item.grocery}</Text>
         <Text style={styles.quantityText}>{item.quantity}</Text>
       </View>
-      <Button title="Remove" onPress={() => handleRemoveGrocery(item.key)} />
+      <Button
+        title="Remove"
+        onPress={() => handleRemoveGrocery(item.key)}
+        color="red"
+      />
     </View>
   );
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Carmelo's Shopping List!</Text>
-      </View>
+      <ImageBackground source={image}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Carmelo's Shopping List!</Text>
+        </View>
+      </ImageBackground>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
           placeholder="Grocery"
           value={grocery}
-          onChangeText={text => setGrocery(text)}
+          onChangeText={(text) => setGrocery(text)}
         />
         <TextInput
           style={styles.input}
           placeholder="Quantity"
           value={quantity}
-          onChangeText={text => setQuantity(text)}
+          onChangeText={(text) => setQuantity(text)}
         />
         <Button title="Add" onPress={handleAddGrocery} />
       </View>
-      <FlatList
-        data={groceryList}
-        renderItem={renderGroceryItem}
-        style={styles.list}
-      />
+      
+        <FlatList
+          data={groceryList}
+          renderItem={renderGroceryItem}
+          style={styles.list}
+        />
+      
     </View>
   );
 }
@@ -65,48 +86,52 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
   },
   header: {
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 20,
+    marginTop: 20,
   },
   headerText: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   inputContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 20,
+    marginTop: 50,
   },
   input: {
     flex: 1,
     paddingHorizontal: 10,
     height: 40,
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     marginRight: 10,
   },
   list: {
     flex: 1,
   },
   groceryItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingHorizontal: 10,
     paddingVertical: 5,
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderRadius: 5,
-    marginBottom: 10,
+    marginBottom: 5,
+    marginTop: 2,
+    backgroundColor: "#ebe5cc",
   },
   itemInfo: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   groceryText: {
     fontSize: 18,
   },
   quantityText: {
     fontSize: 18,
-    color: 'gray',
+    color: "gray",
     marginLeft: 10,
   },
 });
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ImageBackground } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { store, persistor } from './store';
 
+
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <App />
+      
+        <App />
+      
     </PersistGate>
   </Provider>,
   document.getElementById('root')
