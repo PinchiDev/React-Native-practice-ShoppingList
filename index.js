3356cf51b4eeb066d399018f2e0a4c7cd156128d
Carmelo Cabezon <89110064+PinchiDev@users.noreply.github.com>
2023-03-14 15:07:02 -0300

    reparing

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ import { store, persistor } from './store';
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      
         <App />
-      
     </PersistGate>
   </Provider>,
   document.getElementById('root')
